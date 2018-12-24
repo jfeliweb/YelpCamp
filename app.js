d135c67c4da1a402bfc46034d9e0ae55c27efc9f
jeazyV <jeazy@bizdotsmedia.com>
2018-12-24 00:25:00 +0000

    Added Bootstrap layout to page

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    {name: "Pembroke", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104491f5c578a4e8b7ba_340.jpg"},
-    {name: "River Pines", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144590f4c07aa2efb7_340.jpg"},
-    {name: "Rainberry Bay", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f5c578a4e8b7ba_340.jpg"}
+    {name: "Pembroke", image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"},
+    {name: "River Pines", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144590f5c97ea5edb0_340.jpg"},
+    {name: "Rainberry Bay", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"}
     ]
 
 app.get("/", function(req, res) {
