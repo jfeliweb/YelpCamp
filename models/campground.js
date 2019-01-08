ba370fbf2ff880db749000056c2a11ed634bbbab
jfeliweb <jeazy@bizdotsmedia.com>
2019-01-08 11:19:30 +0000

    Move campground schema to its own file

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+var mongoose    = require("mongoose");
+
+// Schema SETUP
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+module.exports = Campground;
