3a04c51b6cea534eb329d444ac2ef91ea309661e
jfeliweb <jeazy@bizdotsmedia.com>
2019-01-11 20:39:37 +0000

    Users are now link with the campgrounds. Also add username to display with created campground and user needs to be login to create campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,18 +15,23 @@ router.get("/", function(req, res) {
 });
 
 //CREATE - Add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     // get data from form and add data to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: description, author: author};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
        if(err){
            console.log(err);
        } else {
         //   redirect to campgrounds list
+           console.log(newlyCreated);
            res.redirect("/campgrounds");
        }
     });
@@ -34,7 +39,7 @@ router.post("/", function(req, res){
 });
 
 //NEW - show form to create new campground 
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
    res.render("campgrounds/new"); 
 });
 
@@ -53,6 +58,12 @@ router.get("/:id", function(req, res) {
     
 });
 
-
+// MIDDLEWARE - check if user is login
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
