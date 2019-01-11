1296db37ab9c0ff41b8fd165a262d56a132d14ea
jfeliweb <jeazy@bizdotsmedia.com>
2019-01-11 20:13:54 +0000

    Comments user now associate with a new commit

--- routes/comments.js
+++ routes/comments.js
@@ -28,10 +28,12 @@ router.post("/", isLoggedIn, function(req, res){
                if(err){
                    console.log(err);
                } else {
-                   // connect new comment to campground
-                   // redirect campground show page
+                   comment.author.id = req.user._id;
+                   comment.author.username = req.user.username;
+                   comment.save();
                    campground.comments.push(comment);
                    campground.save();
+                   console.log(comment);
                    res.redirect("/campgrounds/" + campground._id);
                }
            });
