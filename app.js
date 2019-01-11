47a8d8bb21590af82f29d42022f5e3a34daaffa7
jfeliweb <jeazy@bizdotsmedia.com>
2019-01-11 11:21:09 +0000

    added login template and config

--- app.js
+++ app.js
@@ -148,7 +148,16 @@ app.post("/register", function(req, res) {
         });
     });
 });
-
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+// login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res) {
+});
 
 
 
